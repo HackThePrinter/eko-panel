59bf2e2290bfa5e66cf9574357784dc2ad3bade8
HacKan <hackan@gmail.com>
2017-09-28 03:32:40 -0300

    Add rules page
    Fix mobile view (for real).

--- eko/ScoreboardEKO13/urls.py
+++ eko/ScoreboardEKO13/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-            url(r'^$', views.index, name='index'),
-            ]
+    url(r'^$', views.index, name='index'),
+    url(r'^rules/$', TemplateView.as_view(template_name='ScoreboardEKO13/rules.html'), name='rules'),
+]
