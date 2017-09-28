ffd6fe589d8f3af2ca1d4d4221394ac722049d7b
HacKan <hackan@gmail.com>
2017-09-28 03:34:56 -0300

    Improve deploy script
    Copy database before deploying new build.

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker cp hacktheprinter:/home/docker/code/app/eko/db.sqlite3 "/srv/hacktheprinter/data/db-eko13-$(date +'%s').sqlite3"
+docker cp hacktheprinter:/home/docker/code/app/eko/db.sqlite3 app/eko/db.sqlite3
 docker stop hacktheprinter && docker rm hacktheprinter && docker rmi hacktheprinter
 [[ ! -f "dhparams.pem" ]] && openssl dhparam -out dhparams.pem 4096
 docker build -t hacktheprinter .
